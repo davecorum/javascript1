35553ab4cc08f313cd6a23a71c52a9779ced6dc2
David Corum <nrgdavid710@gmail.com>
2018-07-02 20:37:09 -0700

    Finished Day 1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//DECLARE A VARIABLE
+var firstName;
+
+//DEFINE YOUR VARIABLE
+firstName = "David"; //String
+
+//DEFINE AND DECLARE YOUR VARIABLE
+var lastName = "Corum";
+
+console.log(firstName, lastName);
+
+//SINGLE LINE COMMENT
+
+/*DOUBLE LINE
+          COMMENT*/

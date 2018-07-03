974a1f3982a8a2ab94801d9482948dbba418632d
David Corum <nrgdavid710@gmail.com>
2018-07-02 21:01:23 -0700

    First Push of Day 2

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var myString = "Hellow World!*76"; //String
+console.log(typeof myString);
+var myNumber = 43; //Number
+console.log(typeof myNumber);
+
+var isLoggedIn = false; //Boolean
+
+var newVariable;
+console.log(newVariable); //Undefined
+
+isLoggedIn = null; //Null

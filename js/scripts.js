bd63de42fad8c42827eed294427f9eb007408c77
David Corum <nrgdavid710@gmail.com>
2018-07-18 18:32:51 -0700

    Finished Day 5

--- js/scripts.js
+++ js/scripts.js
@@ -66,11 +66,58 @@ string => (string.toUpperCase());
 string => string.toUpperCase();
 
 //TRY IT OUT #1
-function dogAge(string) {
-  let regularAge = prompt(string);
-  alert(regularAge * 7);
-}
-dogAge("What's your dog's age?")
+// function dogAge(string) {
+//   let regularAge = prompt(string);
+//   alert(regularAge * 7);
+// }
+// dogAge("What's your dog's age?")
+//
+// function calcLifetimeSupply(currentAge, dailyUse) {
+//   const endOfLife = 80;
+//   if(isNaN(currentAge) && isNaN(daily)) {
+//     alert("Please give me a digit!");
+//     let currentAgeSecondTry = prompt("What's your age?");
+//     let dailyUseSecondTry = prompt("How many per year?");
+//     calcLifetimeSupply(currentAgeSecondTry, dailyUseSecondTry);
+//   } else {
+//     if(currentAge <80) {
+//       let supply = (endOfLife - currentAge) *365;
+//       alert(`You will need ${supply} to last until you're ${endOfLife}`);
+//     } else {
+//     alert("This is...awkward...you're dead.");
+//     }
+//   }
+// };
+// let age = prompt("How old are you?");
+// let daily = prompt("How many per year?");
+// calcLifetimeSupply(age, daily);
+
+//METHODS
+console.log(344334); //log was written to consume ARGUMENTS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

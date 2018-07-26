96a7608218b7b15554cf3a5b3f2b9fdb9fc7770d
David Corum <nrgdavid710@gmail.com>
2018-07-25 20:58:46 -0700

    Day 8 Partially Completed

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+let loginForm = document.forms.login;
+let emailInput = loginForm.email;
+
+/*
+THE FOUR MAIN SYMBOLS TO MEMORIZE!!!!!!!!
+? = after this = QUERY PARAMETER
+& = separates parameters
+*/
+
+/*
+TWO OF THE FIVE MAIN VERBS!!!!!!
+GET = You give data, and they don't give back
+POST = User gives data to do something
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//

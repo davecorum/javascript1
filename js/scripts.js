f815ac4ac36f90c21162239d99a0dbff0827fd0a
David Corum <nrgdavid710@gmail.com>
2018-08-06 20:21:25 -0700

    Day 11 Completed

---
+++ js/scripts.js
@@ -0,0 +1,90 @@
+// const weCool = false;
+// const myPromise = new Promise((resolve, reject) => {
+//   if (weCool) resolve("We are in fact cool.")
+//   else if (!weCool) reject("Sorry, we ain't cool...");
+// });
+//
+// myPromise.then(result => {
+//   console.log(result);
+// }).catch(error => {
+//   console.log(error);
+// });
+//
+// function returnTwo() {
+//   return 2;
+// };
+// console.log(Promise.resolve(returnTwo()));
+
+
+
+// let starTrekSeries;
+// fetch("http://stapi.co/api/v1/rest/series/search").then(response => {
+//   // console.log(response);
+//   if (response.ok) {
+//     console.log("yay");
+//   };
+//   return response.json();
+// }).then(jsonResult => {
+//   console.log(jsonResult.series);
+//  starTrekSeries = json.series;
+// }).catch(error);
+
+// fetch("http://localhost:3456", {
+//   method: "POST",
+//   mode: "cors",
+//   headers: {
+//     "Content-Type": "application/json"
+//   },
+//   body: {
+//     number: 1135,
+//     favoriteColor: "blue"
+//   }
+// }).then(response => {
+//   return response.json()
+// }).then(jsonResult => {
+//   console.log(jsonResult)
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
